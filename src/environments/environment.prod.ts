f355652ae4b67e1954b3517836760246fa6ce7d3
vaibhavsinghal87 <vaibhavsinghal87@gmail.com>
2018-10-26 00:14:15 +0530

    update to Angular 7

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDWG14v9MDGp3d1uK--C6H7G0gG9o5uhsQ",
+    authDomain: "angular-concepts-by-examples.firebaseapp.com",
+    databaseURL: "https://angular-concepts-by-examples.firebaseio.com",
+    projectId: "angular-concepts-by-examples",
+    storageBucket: "angular-concepts-by-examples.appspot.com",
+    messagingSenderId: "563725722220"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDWG14v9MDGp3d1uK--C6H7G0gG9o5uhsQ",
+    authDomain: "angular-concepts-by-examples.firebaseapp.com",
+    databaseURL: "https://angular-concepts-by-examples.firebaseio.com",
+    projectId: "angular-concepts-by-examples",
+    storageBucket: "angular-concepts-by-examples.appspot.com",
+    messagingSenderId: "563725722220"
+  }
 };
